8481c8c6c693eadbe6c8085f66d0df49802cd9ab
lanzwerk <georg.lanz@gmail.com>
2017-01-25 12:38:59 +0100

    Update mb2.js
    city parameter now functional
    issues:
    still returns city name in results

--- javascript/mb2.js
+++ javascript/mb2.js
@@ -1,23 +1,9 @@
-/*var app = angular.module("app", []);
 
-app.controller("controller", function($scope, $http) {
-  $scope.nom = "Muse";
-
-  $scope.search = function() {
-    var url = "http://musicbrainz.org/ws/2/artist/?query=artist:" + $scope.nom + "&fmt=json";
-
-    $http.get(url)
-      .then(function(response) {
-        $scope.listenoms = response.data.artists;
-        console.log($scope.listenoms);
-      });
-  };
-});
-*/
 // https://musicbrainz.org/ws/2/artist/?query=area:hamburg
 
 function callMb(city_) {
-  var city = city_
+//  var city = city_
+//  var url = "https://musicbrainz.org/ws/2/artist/?query=area:" + "berlin";
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -26,13 +12,17 @@ function callMb(city_) {
      document.getElementById("output").innerHTML = new_artist;
     }
   };
-  xhttp.open("GET", "https://musicbrainz.org/ws/2/artist/?query=area:berlin", true);
+  var url = genUrl(city_);  //url is generated
+  xhttp.open("GET", url , true);
   xhttp.send();
 }
 
 
-//var xml_string = '<answers><answer id="0">hello</answer><answer id="1">goodbye</answer></answers>';
-//var answers = get_artist_from_xml(xml_string);
+function genUrl(city) {
+	var url = "https://musicbrainz.org/ws/2/artist/?query=area:" + city.value;
+	return url;
+}
+
 
 function get_artist_from_xml(xml) {
     // Parse the XML string into a XMLDocument
@@ -40,16 +30,21 @@ function get_artist_from_xml(xml) {
                 ? new DOMParser().parseFromString(xml_string, 'text/xml')    // Standard
                 : new ActiveXObject('Microsoft.XMLDOM').loadXML(xml_string); // IE
 
-    // Find the artist name nodes
+    // Find the 'name' nodes and save into artist_nodes
     var artist_nodes = doc.getElementsByTagName('name');
-    var artist = [];
-
+/*	var neededElements = [];
+	for (var i = 0, i < artist_nodes.length; i++) {
+ 		  if (artist_nodes[i].parentNode == 'artist') {
+        	neededElements.push(artist_nodes[i])
+    	}
+    }  */
+	var artist = [];
     // Loop through them and save their text content into an array
     for (var i = 0; i < artist_nodes.length; i++) {
-        artist.push(artist_nodes[i].firstChild.data)
+//        if (artist_nodes[i].parentNode == 'artist') {
+        	artist.push(artist_nodes[i].firstChild.data)
+//		}
     }
 	var rand = artist[Math.floor(Math.random() * artist.length)]; //random entry from artist array
     return rand;
 }
-
-
