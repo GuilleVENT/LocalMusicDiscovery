237527369b8cb77585bc9b37ffff5a18b924c924
fplacardona <ferran.pla@cdtm.de>
2017-01-31 18:22:11 +0100

    Integrated artist change via button!

--- web/js/getID.js
+++ web/js/getID.js
@@ -1,5 +1,4 @@
 function getID(artist){
-
     console.log("here");
 
     //var artista = "Morrissey";
@@ -7,7 +6,6 @@ function getID(artist){
 
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
-
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
@@ -27,21 +25,17 @@ function getID(artist){
 
     xhr.send(data);
     console.log(data);
-
 }
 
 function genUrl(artist) {
-
 	var url = "https://api.spotify.com/v1/search?q="+artist+"&type=artist&market=US&limit=1";
     console.log("URL Generated succesfully : ")
     console.log(url);
 	return url;
-
 }
 
 
 function findID(array){
-
     var parsedsearch = array;
     console.log("-------------------------PARSEDSEARCH-------------------------");
     console.log(parsedsearch);
@@ -65,12 +59,10 @@ function findID(array){
     console.log("-------------------------ARTIST ID-------------------------");
     TOPtracks(artist_id);
     return artist_id;
-
 }
 
 
 function TOPtracks(artist_id){
-
     var data = null;
 
     var xhr = new XMLHttpRequest();
@@ -81,11 +73,11 @@ function TOPtracks(artist_id){
             var array = JSON.parse(this.responseText);
             var TOPsongs = [];
             TOPsongs = tracksID(array);
-            document.getElementById('myIframe').src = "https://embed.spotify.com/?uri=spotify:track:" + TOPsongs[2];
+            document.getElementById("output2").innerHTML = TOPsongs;
         }
     });
 
-    var url2 = genUrl2(artist_id);
+     var url2 = genUrl2(artist_id);
     xhr.open("GET", url2, true);
     xhr.setRequestHeader("accept", "application/json");
     //xhr.setRequestHeader("cache-control", "no-cache");
@@ -94,7 +86,6 @@ function TOPtracks(artist_id){
 
     xhr.send(data);
     console.log(data);
-
 }
 
 function genUrl2(artist_id){
@@ -105,7 +96,6 @@ function genUrl2(artist_id){
 }
 
 function tracksID(array){
-
     var tracks_id = [];
     var parsedbody = array;
     console.log("-------------------------PARSEDBODY = ARRAY");
@@ -129,11 +119,4 @@ function tracksID(array){
     console.log(tracks_id);
 
     return tracks_id;
-
-}
-
-function changeSong(){
-
-  getID("John Mayer");
-
 }
